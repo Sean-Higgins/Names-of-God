b79f2ac502607520548ccdce364273a070f125a7
Sean Higgins <seanhiggins639@gmail.com>
2024-04-05 18:16:06 -0700

    Beginning my new project, setting up the Rust and Cargo environment to work out of.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
+const MAX_NAMES: u64 = 9000000000;	// Max number of names of God is nine
+					// billion, which fits into a u32 type
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
